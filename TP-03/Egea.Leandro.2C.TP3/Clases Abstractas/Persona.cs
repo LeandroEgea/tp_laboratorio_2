8b11b185ed49b097fe4ed056db6798bfa03e2c01
Leandro Egea <leandroe@lagash.com>
2019-06-04 12:54:46 -0300

    arranco el TP3

---
+++ TP-03/Egea.Leandro.2C.TP3/Clases Abstractas/Persona.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Abstractas
+{
+    public abstract class Persona
+    {
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+        private string nombre;
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+            set
+            {
+                apellido = value;
+            }
+        }
+
+        public int DNI
+        {
+            get
+            {
+                return dni;
+            }
+            set
+            {
+                throw new NotImplementedException();
+                //TODO
+            }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get
+            {
+                return nacionalidad;
+            }
+            set
+            {
+                nacionalidad = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string StringToDNI
+        {
+            set
+            {
+                throw new NotImplementedException();
+                //TODO
+            }
+        }
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Nacionalidad = nacionalidad;
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            DNI = dni;
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            StringToDNI = dni;
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+            //TODO
+        }
+
+        private int ValidarDNI(ENacionalidad nacionalidad, int dato)
+        {
+            throw new NotImplementedException();
+            //TODO
+        }
+
+        private int ValidarDNI(ENacionalidad nacionalidad, string dato)
+        {
+            throw new NotImplementedException();
+            //TODO
+        }
+
+        private string ValidarNombreApellido(string dato)
+        {
+            throw new NotImplementedException();
+            //TODO
+        }
+    }
+}
